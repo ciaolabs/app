36d5df00471ccbdab6a4dad33c08f711497367c0
Mattia Ippoliti <Ippolitimattia@gmail.com>
2026-05-14 23:37:54 +0200

    Polish survey hero background

--- apps/survey/src/components/home/hero-lottie-bg.tsx
+++ apps/survey/src/components/home/hero-lottie-bg.tsx
@@ -1,8 +1,5 @@
 export function HeroLottieBackground() {
   return (
-    <>
-      <div className="hero-lottie-bg" aria-hidden="true" />
-      <div className="hero-noise" aria-hidden="true" />
-    </>
+    <div className="hero-lottie-bg" aria-hidden="true" />
   );
 }
